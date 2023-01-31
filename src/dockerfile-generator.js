0a56df28690f50dff318148b008c1db51266b807
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2023-01-31 19:13:17 +0100

    enhancement: added prompts for the java, cs and the env variabales as js functions

--- src/dockerfile-generator.js
+++ src/dockerfile-generator.js
@@ -2,6 +2,7 @@ import { createSpinner } from "nanospinner";
 import inquirer from "inquirer";
 import { sleep } from "./utilities/sleep.js";
 import getTemplate from './utilities/getTemplate.js'
+import Mustache from 'mustache'
 
 const JSFrontendApplicationTypes = {
     react: 'React',
@@ -45,16 +46,101 @@ const environmentTypes = {
     production: 'production'
 }
 
+const getEnvironmentVariables = async (nbEnv) => {
+    let enviroment_entries = "";
+    for(let i =0; i< nbEnv; i++) {
+        const envVar = await inquirer.prompt([
+            {
+                message: "What is the environment variable name?",
+                name: 'env_name',
+                type: 'input'
+            }, 
+            {
+                message: "what is the value of this environment variable?",
+                name: 'env_value',
+                type: 'input'
+            }
+        ])
+        enviroment_entries += `ENV ${envVar.env_name}=${envVar.env_value}\n\n`;
+    }
+    return enviroment_entries;
+} 
 
-const javaBasedDockerfileGenerator = async () => {
-
+const javaBasedDockerfileGenerator = async (content) => {
+    const answers = await inquirer.prompt([
+        {
+            message: "What maven version you are working with?",
+            name: 'mvn_version',
+            type: "list",
+            choices: ['3.8', '3.7', '3.6', '3.3']
+        }, 
+        {
+            message: "What JDK version you are working with",
+            name: "jdk_version",
+            type: "list",
+            choices: ['18', '17', '11', '9']
+        }, 
+        {
+            message: "What port are you exposing the application through? (default is 8080)",
+            name: "port",
+            type: "number",
+            default: "8080",
+        },
+        {
+            message: "How many environment variables you want to define? (default is 0)",
+            name: "nb_env",
+            type: 'number',
+            default: 0 
+        }
+    ])
+    const enviroment_entries = getEnvironmentVariables(answers.nb_env);
+    const values = {
+        mvn_version: answers.mvn_version,
+        jdk_version: answers.jdk_version,
+        port: answers.port,
+        environment_variables: enviroment_entries 
+    };
+    return Mustache.render(content, values);
 }
 
-const javascriptBasedDockerfileGenerator = async () => {
-
+const csharpBasedDockerfileGenerator = async (content) => {
+    const answers = await inquirer.prompt([
+        {
+            message: "What dotnet sdk version are you working with?",
+            name: "dotnet_sdk_version",
+            type: "list",
+            choices: ["7.0.2", "6.0.13", "6.0.12"]
+        }, 
+        {
+            message: "What ASP.Net version you want to use?", 
+            name: "aspnet_version",
+            type: "list",
+            choices: ["7.0.2", "6.0.13" ,"6.0.12"]
+        },
+        {
+            message: "What port are you exposing the application through? (default is 8080)",
+            name: "port",
+            type: "number",
+            default: "8080",
+        }, 
+        {
+            message: "How many environment variables you want to define? (default is 0)",
+            name: 'nb_env',
+            type: 'number',
+            default: 0
+        }
+    ]);
+    const environment_entries = getEnvironmentVariables(answers.nb_env);
+    const values = {
+        dotnet_sdk_version: answers.dotnet_sdk_version,
+        aspnet_version: answers.aspnet_version,
+        port: answers.port,
+        environment_variables: environment_entries
+    };
+    return Mustache.render(content, values);
 }
 
-const csharpBasedDockerfileGenerator = async () => {
+const javascriptBasedDockerfileGenerator = async (content, appType) => {
 
 }
 
@@ -67,7 +153,7 @@ const dockerfileGenerator = async () => {
     const spinner = createSpinner('Docker file generator').start();
     await sleep(1000);
     spinner.stop();
-    const applivationTypeAnswer = await inquirer.prompt({
+    const applicationTypeAnswer = await inquirer.prompt({
         name: 'application_type',
         message: 'what kind of application are you working on',
         type: 'list',
@@ -83,9 +169,31 @@ const dockerfileGenerator = async () => {
         })
         const envType = environmentTypeAnswer.application_env;
         const content = await getTemplate(appType, envType);
-        console.log(content);
+        const generatorSpinner = createSpinner(`${appType} dockerfile generation in progress`).start();
+        await sleep(1000);
+        generatorSpinner.stop();
+        let result ="";
+        if(Object.values(JavaapplicationTypes).includes(appType)) {
+            result = await javaBasedDockerfileGenerator(content);
+        } else if(Object.values(PythonApplicationTypes).includes(appType)){
+            result = await pythonBasedDockerfileGenerator(content);
+        } else {
+            result = await javascriptBasedDockerfileGenerator(content, appType)
+        }
+        generatorSpinner.success(`${appType} dockerfile generated successfully`);
+        console.log("\n\n")
+        console.log(result)
+        console.log("\n\n")
     } else {
-        // Handle Dotnet
+        const content = await getTemplate(appType, 'dockerfile');
+        const csSpinner = createSpinner('Dotnetdockerfile generation in progress').start();
+        await sleep(1000);
+        csSpinner.stop();
+        const result =  await csharpBasedDockerfileGenerator(content);
+        csSpinner.success('Dotnet dockerfile generated successfully')
+        console.log("\n\n")
+        console.log(result)
+        console.log("\n\n")
     }
 }
 
